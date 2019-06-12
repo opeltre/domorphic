95d8d3c18486d74c7acef871c921d7973d21012c
Olivier Peltre <opeltre@gmail.com>
2019-06-12 23:12:38 +0200

    browserify bundle

--- bundler.bash
+++ bundler.bash
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-deps=('__' 'fst' '_fst' 'elements' 'ajax' 'export')
-bundle='bundle.js'
-#dir='/home/shevket/js/vv/'
-
-echo '/* forest bundle */' > $(pwd)/$bundle && echo "bundling..."
-
-for d in ${deps[*]}
-do  
-    cat $(pwd)/src/$d.js >> $(pwd)/$bundle && echo "->  $d.js"
-done
-
-echo "...done"
+browserify -e SRC/dom.js -s bundle -o ./bundle.js
