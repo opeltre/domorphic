0b3d6ea8778d4b5e4122ad53d8f1590cede2fce9
Olivier Peltre <opeltre@gmail.com>
2020-04-21 10:33:04 +0200

    remove update dep from this branch

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 let dom = require('./src/dom'),
     IO = require('./src/io'),
-    state = require('./src/state'),
-    update = require('./src/update');
+    state = require('./src/state');
 
 module.exports = Object.assign(dom, 
     {IO},
-    {state},
-    {update}
+    {state}
 );
