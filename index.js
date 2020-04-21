707ba60e1e054e65cbd78fff00c8770ccccd4d3f
Olivier Peltre <opeltre@gmail.com>
2020-04-21 13:41:56 +0200

    export update: e -> St(s, IO e)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let dom = require('./src/dom'),
     IO = require('./src/io'),
+    update = require('./src/update'),
     state = require('./src/state');
 
 module.exports = Object.assign(dom, 
     {IO},
-    {state}
+    {state},
+    {update}
 );
